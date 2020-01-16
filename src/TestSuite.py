b29ff4d33aa25a130d208ab7d3440a5c571de8dd
blairryan <rblairr20@gmail.com>
2020-01-16 02:18:59 -0800

    added test coverage for DirectoryHandler.directory_exists; added new test case object to existing test suite

--- src/TestSuite.py
+++ src/TestSuite.py
@@ -1,5 +1,7 @@
 import unittest
 from test_FileHasher import TestFileHasher
+from test_DirectoryHandler import TestDirectoryHandler
+
 
 def test_suite():
     """
@@ -7,8 +9,10 @@ def test_suite():
     """
     test_suite = unittest.TestSuite()
     test_suite.addTest(unittest.makeSuite(TestFileHasher))
+    test_suite.addTest(unittest.makeSuite(TestDirectoryHandler))
     return test_suite
 
-my_test_suite = test_suite()
-runner=unittest.TextTestRunner()
-runner.run(my_test_suite)
+if __name__ == "__main__":
+    my_test_suite = test_suite()
+    runner=unittest.TextTestRunner()
+    runner.run(my_test_suite)
---
+++ src/test_DirectoryHandler.py
@@ -0,0 +1,42 @@
+import unittest
+import hashlib
+from unittest import mock
+from DirectoryHandler import DirectoryHandler
+import os
+
+class TestDirectoryHandler(unittest.TestCase):
+    def setUp(self):
+        self.directory_handler = DirectoryHandler("fake dir path")
+
+# DirectoryHandler.directory_exists test cases
+
+    def test_directory_exists_returns_bool(self):
+        """
+            Ensure the return type of directory_exists is of type bool.
+        """
+        dir_exists = self.directory_handler.directory_exists()
+        assert isinstance(dir_exists, bool)
+
+    def test_directory_not_exists_(self):
+        """
+            Ensure directory_exists returns False when the 
+            directory does not exist.
+        """
+        dir_exists = self.directory_handler.directory_exists()
+
+        self.assertFalse(dir_exists)
+
+    @mock.patch("DirectoryHandler.get_directory", return_value=os.getcwd())
+    def test_directory_exists(self):
+        """
+            Ensure directory_exists returns True when the directory exists.
+        """
+        dir_exists = self.directory_handler.directory_exists()
+
+        self.assertTrue(dir_exists)
+
+
+
+
+
+    
