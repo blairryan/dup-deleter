5c3ad3d1ee734060afbc61a189ad20c27f8242f3
blairryan <rblairr20@gmail.com>
2020-01-22 01:18:25 -0800

    added docstrings to methods; check is_duplicate before updating all_files in consume_files

--- src/DuplicateTracker.py
+++ src/DuplicateTracker.py
@@ -9,15 +9,25 @@ class DuplicateTracker(object):
         self.duplicate_files = set()
 
     def consume_files(self, files: Set[str], file_root: str) -> None:
+        """
+            Hash and keep track of all files, separate out duplicates into their
+            own container.
+        """
         for f in files:
             file_path = os.path.join(file_root, f)
             file_hasher = FileHasher(file_path)
             hash_value = file_hasher.generate_hash()
+            if self.is_duplicate(hash_value): self.duplicate_files.add(f)
             self.all_files.update({f: hash_value})
-            if self.is_duplicate(f): self.duplicate_files.add(f)
 
-    def is_duplicate(self, file: str) -> bool:
+    def is_duplicate(self, hash_value: str) -> bool:
+        """
+            Return True if the given file is considered a duplicate.
+        """
         pass
 
     def get_duplicates(self) -> Set[str]:
+        """
+            Return the list of duplicate files.
+        """
         pass
