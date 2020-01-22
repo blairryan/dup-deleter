9900163907d81dc8b7a7f05d3eeef5f9a6c69465
blairryan <rblairr20@gmail.com>
2020-01-21 23:51:39 -0800

    added functionality for consume_files

--- src/DuplicateTracker.py
+++ src/DuplicateTracker.py
@@ -9,7 +9,12 @@ class DuplicateTracker(object):
         self.duplicate_files = set()
 
     def consume_files(self, files: Set[str], file_root: str) -> None:
-        pass
+        for f in files:
+            file_path = os.path.join(file_root, f)
+            file_hasher = FileHasher(file_path)
+            hash_value = file_hasher.generate_hash()
+            self.all_files.update({f: hash_value})
+            if self.is_duplicate(f): self.duplicate_files.add(f)
 
     def is_duplicate(self, file: str) -> bool:
         pass
